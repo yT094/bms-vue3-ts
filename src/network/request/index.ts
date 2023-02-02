34698ad21d2bda41ec11e41f3547a209f290ef5d
coderycs <1748780248@qq.com>
2023-02-02 19:28:02 +0800

    feat: 添加拦截器和 loading 效果

--- src/network/request/index.ts
+++ src/network/request/index.ts
@@ -1,11 +1,46 @@
 import axios from "axios";
 // 如果确定导入的是类型，可用 type 说明
 import type { AxiosInstance, AxiosRequestConfig } from "axios";
+import { ElLoading } from "element-plus";
+import type { ILoadingInstance } from "element-plus/lib/el-loading/src/loading.type";
 
 class JNRequest {
   instance: AxiosInstance;
+  // 设置可选项，可避开 ts 的类型检查
+  loading?: ILoadingInstance;
+
   constructor(config: AxiosRequestConfig) {
     this.instance = axios.create(config);
+
+    this.instance.interceptors.request.use(
+      (config) => {
+        // 遮罩层
+        this.loading = ElLoading.service({
+          lock: true,
+          text: "正在请求数据...",
+          background: "rgba(0,0,0,0.5)",
+        });
+        return config;
+      },
+      (err) => {
+        return err;
+      }
+    );
+
+    this.instance.interceptors.response.use(
+      (res) => {
+        // 关闭遮罩层
+        this.loading?.close();
+        // 处理响应数据
+        const data = res.data;
+        return data;
+      },
+      (err) => {
+        // 关闭遮罩层
+        this.loading?.close();
+        return err;
+      }
+    );
   }
 
   request(config: AxiosRequestConfig): void {
