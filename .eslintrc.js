de7b4d39fe4159a364d09ab932be5f0224219101
coderycs <1748780248@qq.com>
2023-02-09 19:48:35 +0800

    fix: 关闭 eslint 检查文件名是否为驼峰命名

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // 关闭eslint检查文件名是否为驼峰命名
+    "vue/multi-word-component-names": "off",
   },
 };
