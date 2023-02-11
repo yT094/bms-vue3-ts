314428ae2613349d3a547c0aa9d3e8cfa507b213
coderycs <1748780248@qq.com>
2023-02-11 21:05:18 +0800

    feat: 搭建手机号登录页

--- src/views/login/cpns/login-phone.vue
+++ src/views/login/cpns/login-phone.vue
@@ -1,15 +1,40 @@
 <template>
-  <div class="login-phone">手机号登录</div>
+  <div class="login-phone">
+    <el-form :model="ruleForm" :rules="phoneRules">
+      <el-form-item label="手机号" prop="phone">
+        <el-input v-model="ruleForm.phone"></el-input>
+      </el-form-item>
+
+      <el-form-item label="验证码" prop="verificationCode">
+        <div class="e-getcode">
+          <el-input v-model="ruleForm.verificationCode"></el-input>
+          <el-button type="primary" class="ml15">获取验证码</el-button>
+        </div>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
+import { phoneRules } from "../config/config";
 
 export default defineComponent({
   setup() {
-    return {};
+    const ruleForm = reactive({
+      phone: "",
+      verificationCode: "",
+    });
+    return { ruleForm, phoneRules };
   },
 });
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.login-phone {
+  .e-getcode {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
