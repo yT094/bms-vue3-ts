ffd11cf57579712cd2779030afff6a41bf68c196
coderycs <1748780248@qq.com>
2023-02-12 15:54:09 +0800

    feat: 封装 LocalCache 方法

---
+++ src/utils/cache.ts
@@ -0,0 +1,23 @@
+class LocalCache {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  setCache(key: string, value: any) {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  getCache(key: string) {
+    const value = window.localStorage.getItem(key);
+    if (value) {
+      return JSON.parse(value);
+    }
+  }
+
+  deleteCache(key: string) {
+    window.localStorage.removeItem(key);
+  }
+
+  clearCache() {
+    window.localStorage.clear();
+  }
+}
+
+export default new LocalCache();
